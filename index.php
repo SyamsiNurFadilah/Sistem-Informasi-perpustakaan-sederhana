1c24b18aa38dcff537f822a95c7ac471e6f111ba
SyamsiNurFadilah <syamsifadilah5@gmail.com>
2024-05-17 13:25:52 +0700

    commit lanjutan

--- index.php
+++ index.php
@@ -6,6 +6,17 @@
     <title>Perpustakaan Jakarta</title>
 </head>
 <body>
-    
+    <header>
+        <h3>Perpustakaan Jakarta</h3>
+        <ul>
+            <li><a href="signup.php">Sign Up</a></li>
+            <li><a href="signin.php">Sign In</a></li>
+        </ul>
+    </header>
+    <hr>
+    <main>
+        <p>Pinjam buku hanya dalam genggaman,<br>web ini dibuat untuk memudahkan warga Jakarta dalam meminjam buku dimanapun dan kapanpun.</p>
+    </main>
+    <hr>
 </body>
 </html>
